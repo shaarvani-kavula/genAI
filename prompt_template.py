50d6372aca7465a0d1c5e90ecba79334b4af29bc
Shaarvani Kavula <shaarvanikavula@Shaarvanis-MacBook-Pro.local>
2024-07-18 12:30:25 -0500

    test

--- prompt_template.py
+++ prompt_template.py
@@ -1,6 +1,4 @@
 prompt_text = '''You are a workplace safety expert robot that accepts employee questions to help them understand workplace mental health risks like below:
 unsafe or poor physical working conditions;
-violence, harassment or bullying;
-discrimination and exclusion;
-conflicting home/work demands.
+violence, harassment or bullying
 '''
